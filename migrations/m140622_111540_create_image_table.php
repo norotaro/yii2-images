1b71bb512f8dadbd8594eef511e2311ad877f69a
norotaro <nelson.otazo@gmail.com>
2015-10-20 11:29:08 -0300

    Update m140622_111540_create_image_table.php
    Make migration sintax to be independent to de database platform

--- migrations/m140622_111540_create_image_table.php
+++ migrations/m140622_111540_create_image_table.php
@@ -7,20 +7,18 @@ class m140622_111540_create_image_table extends \yii\db\Migration
     public function up()
     {
         $this->createTable('image', [
-            'id' => 'pk',
-            'filePath' => 'VARCHAR(400) NOT NULL',
-            'itemId' => 'int(20) NOT NULL',
-            'isMain' => 'int(1)',
-            'modelName' => 'VARCHAR(150) NOT NULL',
-            'urlAlias' => 'VARCHAR(400) NOT NULL',
+            'id' => $this->primaryKey(),
+            'filePath' => $this->string(400)->notNull(),
+            'itemId' => $this->integer(20)->notNull(),
+            'isMain' => $this->smallInteger(1),
+            'modelName' => $this->string(150)->notNull(),
+            'urlAlias' => $this->string(400)->notNull(),
         ]);
 
     }
 
     public function down()
     {
-        echo "m140622_111540_create_image_table cannot be reverted.\n";
-
-        return false;
+        $this->dropTable('image');
     }
 }
